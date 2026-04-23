a9e0e79ef00e4924bb3e65cc04599790c37cb4a4
unknown <beloussova.d@gmail.com>
2026-04-23 14:15:45 +0500

    design fixes

--- src/app/pages/consultation/consultation.ts
+++ src/app/pages/consultation/consultation.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consultation',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './consultation.html',
   styleUrl: './consultation.css',
 })
